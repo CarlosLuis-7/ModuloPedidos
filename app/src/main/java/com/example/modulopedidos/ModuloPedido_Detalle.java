919a4a748aa161e8c16169a06b23df516aa43277
Carlos Luis <carlitowebdesigner@gmail.com>
2020-01-29 21:33:06 -0500

    proyectoComidas

---
+++ app/src/main/java/com/example/modulopedidos/ModuloPedido_Detalle.java
@@ -0,0 +1,14 @@
+package com.example.modulopedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModuloPedido_Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulo_pedido__detalle);
+    }
+}
